8a5ff53aa390688742d3acd3604ed3f762cf2c3f
Hendra Manudinata <hendra32os@gmail.com>
2021-02-01 20:43:43 +0800

    twrp.sh: Claim more diskspace
    Thanks to @yukosky

--- twrp.sh
+++ twrp.sh
@@ -59,6 +59,24 @@ setup_env() {
 	sudo rm -rf /var/cache/apt/*
 	sudo rm -rf /var/lib/apt/lists/*
 	sudo rm -rf /tmp/*
+
+	# Claim more diskspace
+	# Thanks to @yukosky
+
+	sudo swapoff -av
+	sudo rm -f /swapfile
+
+	sudo rm -rf /usr/share/dotnet
+
+	sudo rm -rf "/usr/local/share/boost"
+	sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+	sudo swapoff -av
+	sudo rm -rf /mnt/swapfile
+	sudo apt-get remove -y '^ghc-8.*' '^dotnet-.*' '^llvm-.*' 'php.*' azure-cli google-cloud-sdk '^google-.*' hhvm google-chrome-stable firefox '^firefox-.*' powershell mono-devel '^account-plugin-.*' account-plugin-facebook account-plugin-flickr account-plugin-twitter account-plugin-windows-live aisleriot brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut totem totem-common totem-plugins printer-driver-brlaser printer-driver-foo2zjs printer-driver-foo2zjs-common printer-driver-m2300w printer-driver-ptouch printer-driver-splix
+	# Bonus
+	sudo dd if=/dev/zero of=swap bs=4k count=1048576
+	sudo mkswap swap
+	sudo swapon swap
 }
 
 # Clone source
